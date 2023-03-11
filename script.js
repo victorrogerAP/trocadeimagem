7fec48c4b091f940096a8676cb6802ce327dd725
victor <73676087+victorrogerAP@users.noreply.github.com>
2023-03-11 18:37:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function imag(){
+    document.getElementById("img").src = "imagens/vamos.JPG";
+}
+function imag1(){
+    document.getElementById("img").src = "imagens/imagem2.JPG";
+}
+function imag2(){
+    document.getElementById("img").src = "imagens/imagem3.JPG";
+}
+function imag3(){
+    document.getElementById("img").src = "imagens/imagem4.JPG";
+}
